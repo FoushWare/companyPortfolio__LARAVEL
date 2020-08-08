15032865df422b96216907cc41e043bb8028c856
FoushWare <mr.fouad992@gmail.com>
2020-08-08 18:45:46 +0200

    styling and logo for the FE

--- FrontEnd/layouts/default.vue
+++ FrontEnd/layouts/default.vue
@@ -1,98 +1,98 @@
-
 <template>
-  <div class="layout">
-    <Layout>
-      <Header>
-        <Menu mode="horizontal" theme="dark" active-name="1">
-          <div class="layout-logo"></div>
-          <div class="layout-nav">
-            <MenuItem name="1">
-              <NuxtLink to="/">
-                <Icon type="ios-navigate"></Icon>Home
-              </NuxtLink>
-            </MenuItem>
-            <MenuItem name="2">
-              <NuxtLink to="/services">
-                <Icon type="ios-keypad"></Icon>Services
-              </NuxtLink>
-            </MenuItem>
-
-            <MenuItem name="3">
-              <NuxtLink to="/teams">
-                <Icon type="ios-analytics"></Icon>Teams
-              </NuxtLink>
-            </MenuItem>
-
-            <MenuItem name="4">
-              <NuxtLink to="/our_works">
-                <Icon type="ios-paper"></Icon>Our Works
-              </NuxtLink>
-            </MenuItem>
-
-            <MenuItem name="5">
-              <NuxtLink to="/pages/1">
-                <Icon type="ios-paper"></Icon>About Us
-              </NuxtLink>
-            </MenuItem>
-
-            <MenuItem name="6">
-              <NuxtLink to="/pages/3">
-                <Icon type="ios-paper"></Icon>Privacy
-              </NuxtLink>
-            </MenuItem>
-
-            <MenuItem name="7">
-              <NuxtLink to="/pages/2">
-                <Icon type="ios-paper"></Icon>Terms And Conditons
-              </NuxtLink>
-            </MenuItem>
-
-            <MenuItem name="8">
-              <NuxtLink to="/clients">
-                <Icon type="ios-paper"></Icon>Clients
-              </NuxtLink>
-            </MenuItem>
-          </div>
-        </Menu>
-      </Header>
-      <Content :style="{padding: '0 50px',marginTop:'20px'}">
-        <Card>
-          <div style="min-height: 200px;">
-            <Nuxt />
-          </div>
-        </Card>
-      </Content>
-      <Footer class="layout-footer-center">2011-2016 &copy; TalkingData</Footer>
-    </Layout>
-  </div>
+    <div class="layout">
+        <Layout>
+            <Header>
+                <Menu mode="horizontal" theme="dark" active-name="1">
+                    <div class="layout-logo">
+                        <img src="foushware.png" alt="" width="130">
+                    </div>
+                    <div class="layout-nav">
+                        <MenuItem name="1">
+                            <NuxtLink to="/" class="white">
+                                Home
+                            </NuxtLink>
+                        </MenuItem>
+                        <MenuItem name="2">
+                            <NuxtLink to="/services" class="white">
+                                Services
+                            </NuxtLink>
+                        </MenuItem>
+                        <MenuItem name="3">
+                            <NuxtLink to="/teams" class="white">
+                                Teams
+                            </NuxtLink>
+                        </MenuItem>
+                        <MenuItem name="4">
+                            <NuxtLink to="/our_works" class="white">
+                                Our Works
+                            </NuxtLink>
+                        </MenuItem>
+                        <MenuItem name="4">
+                            <NuxtLink to="/clients" class="white">
+                                Clients
+                            </NuxtLink>
+                        </MenuItem>
+                        <MenuItem name="4">
+                            <NuxtLink to="/pages/1" class="white">
+                                About us
+                            </NuxtLink>
+                        </MenuItem>
+                        <MenuItem name="4">
+                            <NuxtLink to="/pages/2" class="white">
+                                Privacy
+                            </NuxtLink>
+                        </MenuItem>
+                        <MenuItem name="4">
+                            <NuxtLink to="/pages/3" class="white">
+                                Terms and condtions
+                            </NuxtLink>
+                        </MenuItem>
+                    </div>
+                </Menu>
+            </Header>
+            <Content style="padding: 0 50px ;margin-top:20px">
+                <Card>
+                    <div style="min-height: 200px;">
+                        <Nuxt/>
+                    </div>
+                </Card>
+            </Content>
+            <Footer class="layout-footer-center">2020-2021 &copy; 5dmat-web.com</Footer>
+        </Layout>
+    </div>
 </template>
 <script>
-export default {};
+    export default {
+        head() {
+            return {
+                title: '5dmat-web',
+            }
+        }
+    }
 </script>
 <style scoped>
-.layout {
-  border: 1px solid #d7dde4;
-  background: #f5f7f9;
-  position: relative;
-  border-radius: 4px;
-  overflow: hidden;
-}
-.layout-logo {
-  width: 100px;
-  height: 30px;
-  background: #5b6270;
-  border-radius: 3px;
-  float: left;
-  position: relative;
-  top: 15px;
-  left: 20px;
-}
-.layout-nav {
-  margin: 0 10px 0 10px;
-  margin-right: 20px;
-}
-.layout-footer-center {
-  text-align: center;
-}
+    .layout {
+        background: #f5f7f9;
+        position: relative;
+        overflow: hidden;
+    }
+    .layout-logo {
+        width: 100px;
+        height: 30px;
+        float: left;
+        position: relative;
+        top: 15px;
+        left: 20px;
+        margin-right: 40px;
+    }
+    .layout-nav {
+        margin: 0 10px 0 10px;
+        margin-right: 20px;
+    }
+    .layout-footer-center {
+        text-align: center;
+    }
+    .white{
+        color: white;
+    }
 </style>
-

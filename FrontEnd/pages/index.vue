fbad4d5c0f192cf037170790f686e245a8ae0612
FoushWare <mr.fouad992@gmail.com>
2020-08-08 18:27:02 +0200

    :trollface:  FE Home Page

--- FrontEnd/pages/index.vue
+++ FrontEnd/pages/index.vue
@@ -1,14 +1,25 @@
 <template>
-  <div class="container">
-    <div>
-      <p>HOme Page</p>
-      
+    <div class="container">
+        <div>
+            <Slider />
+            <Service />
+            <Test />
+            <Contact />
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-
+    import Slider from '../components/home/slider'
+    import Test from '../components/home/test'
+    import Service from '../components/home/service'
+    import Contact from '../components/home/contact'
+    export default {
+        comments: {
+            Slider,
+            Test,
+            Service,
+            Contact
+        }
+    }
 </script>
-
-
